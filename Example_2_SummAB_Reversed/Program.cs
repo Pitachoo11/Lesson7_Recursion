021eeabb5fd4ff4e5f18744605fd43c68a16af9e
Petr.Churkin <Petr.Churkin@gmail.com>
2022-11-29 13:09:21 +0300

    Example_2_SummAB_Reversed

---
+++ Example_2_SummAB_Reversed/Program.cs
@@ -0,0 +1,19 @@
+﻿//Собрать строку чисел от a до b, a >= b
+
+
+string NumbersFor(int a, int b)
+{
+string result = String.Empty;
+for (int i = b; i >= a; i--)
+{
+result += $"{i} ";
+}
+return result;
+}
+string NumbersRec(int a, int b)
+{
+if (a <= b) return NumbersRec(a + 1, b) + $"{a} ";
+else return String.Empty;
+}
+Console.WriteLine(NumbersFor(1, 10)); // 10 9 8 7 6 5 4 3 2 1
+Console.WriteLine(NumbersRec(1, 10)); // 10 9 8 7 6 5 4 3 2 1   
